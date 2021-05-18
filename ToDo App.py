1892de5b35330466a324b5883d5269f3c60023f5
Cool_Tkinter_Projects <crystaled2003@gmail.com>
2021-05-18 12:24:20 +0530

    Responsive ToDo App

--- ToDo App.py
+++ ToDo App.py
@@ -1,5 +1,8 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sqlite3
+import pyautogui
+
+a=pyautogui.size() # Return a tuple (width,height) of screen.
 
 # Create Database
 mydb=sqlite3.connect('todo list.db')
@@ -9,16 +12,19 @@ cursor.execute(s)
 mydb.commit() #Commit Changes
 mydb.close() #Close Connection
 
+window_width=int(0.365625*a[0])
+window_height=int(0.6138*a[1])
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(468, 442)
-        MainWindow.setMinimumSize(QtCore.QSize(468, 442))
-        MainWindow.setMaximumSize(QtCore.QSize(468, 442))
+        MainWindow.resize(window_width,window_height)
+        MainWindow.setMinimumSize(QtCore.QSize(window_width,window_height))
+        MainWindow.setMaximumSize(QtCore.QSize(window_width,window_height))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(10, 10, 451, 41))
+        self.lineEdit.setGeometry(QtCore.QRect(int(0.021*window_width), int(0.022*window_height), int(0.96*window_width), int(0.092*window_height)))
         font = QtGui.QFont()
         font.setFamily("Agency FB")
         font.setPointSize(20)
@@ -28,20 +34,20 @@ class Ui_MainWindow(object):
         self.lineEdit.setText("")
         self.lineEdit.setObjectName("lineEdit")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget,clicked=lambda:self.add_item())
-        self.pushButton.setGeometry(QtCore.QRect(10, 60, 131, 31))
+        self.pushButton.setGeometry(QtCore.QRect(int(0.021*window_width), int(0.135*window_height), int(0.279*window_width), int(0.070*window_height)))
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget,clicked=lambda:self.delete_item())
-        self.pushButton_2.setGeometry(QtCore.QRect(150, 60, 161, 31))
+        self.pushButton_2.setGeometry(QtCore.QRect(int(0.320*window_width), int(0.135*window_height), int(0.344*window_width), int(0.070*window_height)))
         self.pushButton_2.setObjectName("pushButton_2")
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget,clicked=lambda:self.save_to())
-        self.pushButton_3.setGeometry(QtCore.QRect(320, 60, 141, 31))
+        self.pushButton_3.setGeometry(QtCore.QRect(int(0.683*window_width), int(0.135*window_height), int(0.301*window_width), int(0.070*window_height)))
         self.pushButton_3.setObjectName("pushButton_3")
         self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
-        self.progressBar.setGeometry(QtCore.QRect(10, 310, 451, 31))
+        self.progressBar.setGeometry(QtCore.QRect(int(0.021*window_width), int(0.701*window_height), int(0.963*window_width), int(0.070*window_height)))
         self.progressBar.setProperty("value", 0)
         self.progressBar.setObjectName("progressBar")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(10, 380, 281, 41))
+        self.label.setGeometry(QtCore.QRect(int(0.021*window_width), int(0.859*window_height), int(0.600*window_width), int(0.092*window_height)))
         font = QtGui.QFont()
         font.setFamily("Agency FB")
         font.setPointSize(20)
@@ -50,7 +56,7 @@ class Ui_MainWindow(object):
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.listWidget = QtWidgets.QListWidget(self.centralwidget)
-        self.listWidget.setGeometry(QtCore.QRect(10, 100, 451, 201))
+        self.listWidget.setGeometry(QtCore.QRect(int(0.021*window_width), int(0.226*window_height), int(0.963*window_width), int(0.454*window_height)))
         font = QtGui.QFont()
         font.setFamily("Agency FB")
         font.setPointSize(20)
@@ -59,7 +65,7 @@ class Ui_MainWindow(object):
         self.listWidget.setFont(font)
         self.listWidget.setObjectName("listWidget")
         self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget,clicked=lambda:self.cleardatabase())
-        self.pushButton_4.setGeometry(QtCore.QRect(10, 350, 451, 31))
+        self.pushButton_4.setGeometry(QtCore.QRect(int(0.021*window_width), int(0.791*window_height), int(0.963*window_width), int(0.070*window_height)))
         self.pushButton_4.setObjectName("pushButton_4")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -114,38 +120,38 @@ class Ui_MainWindow(object):
     # Save ToDo List to a Database
     def save_to(self):
         if self.listWidget.count()==0:
-            self.label.setText('Nothing to Save.')
-            QtCore.QTimer.singleShot(1500,self.clear_label)
-            return
+        	self.label.setText('Nothing to Save.')
+        	QtCore.QTimer.singleShot(1500,self.clear_label)
+        	return
         else:
-            global i
+	        global i
 
-            self.progressBar.setProperty('value',self.i)
-            if self.i!=100:
-                self.i+=1
-                QtCore.QTimer.singleShot(10,self.save_to)
-            else:
-                self.label.setText('Successfully Saved.')
-                self.progressBar.setProperty('value',0)
-                self.i=1
+	        self.progressBar.setProperty('value',self.i)
+	        if self.i!=100:
+	            self.i+=1
+	            QtCore.QTimer.singleShot(10,self.save_to)
+	        else:
+	            self.label.setText('Successfully Saved.')
+	            self.progressBar.setProperty('value',0)
+	            self.i=1
 
-                mydb=sqlite3.connect('todo list.db')
-                cursor=mydb.cursor()
+	            mydb=sqlite3.connect('todo list.db')
+	            cursor=mydb.cursor()
 
-                cursor.execute('delete from todolist')
-    
-                items=[]
+	            cursor.execute('delete from todolist')
+	
+	            items=[]
 
-                for i in range(self.listWidget.count()):
-                    values=self.listWidget.item(i)
-                    values=values.text()
-                    items.append((values,))
-                cursor.executemany('insert into todolist values(?)',items)
-                mydb.commit()
-                mydb.close()
+	            for i in range(self.listWidget.count()):
+	                values=self.listWidget.item(i)
+	                values=values.text()
+	                items.append((values,))
+	            cursor.executemany('insert into todolist values(?)',items)
+	            mydb.commit()
+	            mydb.close()
 
-                QtCore.QTimer.singleShot(1500,self.clear_label)
-                return
+	            QtCore.QTimer.singleShot(1500,self.clear_label)
+	            return
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
